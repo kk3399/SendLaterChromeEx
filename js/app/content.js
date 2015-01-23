5d070336d0c30e609c2c4da3c481537b44d09eea
Krishna Damarla <damarla.kk@gmail.com>
2015-01-23 07:09:09 -0600

    email UI

--- js/app/content.js
+++ js/app/content.js
@@ -3,25 +3,11 @@
 chrome.extension.onMessage.addListener(
 function (request, sender, sendResponse) {
 
-    debugger;
+    //debugger;
 
 
-    if (request.action == 'PageInfo') {
-        var pageInfos = [];
-
-        $('a').each(function() {
-            var pageInfo = {};
-            
-            var href = $(this).attr('href');
-
-            if (href != null && href.indexOf("http") == 0)
-            {
-                //only add urls that start with http
-                pageInfo.url = href
-                pageInfos.push(pageInfo);
-            }
-        });
-
-        sendResponse(pageInfos);
+    if (request.action == 'GET_URL') {
+        var pageURL = document.URL;
+        sendResponse(pageURL);
     }
 });
