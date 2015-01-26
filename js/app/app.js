e10c19d987e8b5afdfcefe190a74a6c6586d10ba
Krishna Damarla <damarla.kk@gmail.com>
2015-01-25 22:50:24 -0600

    dependencies

--- js/app/app.js
+++ js/app/app.js
@@ -1,1 +1,2 @@
-﻿var myApp = angular.module('SendLater', []);
+﻿var myApp = angular.module('SendLater', ['ui.bootstrap.datetimepicker']);
+myApp.value('API_KEY', 'api:key-63466f101367740c78726160f8725dc3');
